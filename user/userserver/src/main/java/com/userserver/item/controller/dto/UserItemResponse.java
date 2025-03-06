ce4cdf3a4ef8300f76342174e1ceadb958e2952d
Kim Se Eun <rlatpdms1014@gmail.com>
2025-03-06 10:43:43 +0900

    feat: User Server에서 user id로 등록한 item 목록 조회 기능

--- user/userserver/src/main/java/com/userserver/item/controller/dto/UserItemResponse.java
+++ user/userserver/src/main/java/com/userserver/item/controller/dto/UserItemResponse.java
@@ -1,4 +1,13 @@
 package com.userserver.item.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserItemResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private int quantity;
 }
--- user/userserver/src/main/java/com/userserver/item/repository/ItemRepository.java
+++ user/userserver/src/main/java/com/userserver/item/repository/ItemRepository.java
@@ -1,7 +1,11 @@
-package com.itemservice.repository;
 
-import com.itemservice.model.entity.Item;
+package com.userserver.item.repository;
+
+import com.userserver.item.model.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findBySellerId(Long sellerId);
 }
