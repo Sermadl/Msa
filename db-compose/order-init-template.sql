eca981af081457ee65f50dd57bd043263635d18f
Sermadl <rlatpdms1014@gmail.com>
2025-03-15 02:44:52 +0900

    refactor: Federated 엔진 미사용으로 인한 불필요한 코드 주석처리

--- db-compose/order-init-template.sql
+++ db-compose/order-init-template.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE IF NOT EXISTS `${ORDER_DATABASE}`;
 USE `${ORDER_DATABASE}`;
 
-CREATE TABLE IF NOT EXISTS `${ORDER_TABLE}` (
-      `quantity` int(11) NOT NULL,
-      `customer_id` bigint(20) DEFAULT NULL,
-      `id` bigint(20) NOT NULL AUTO_INCREMENT,
-      `item_id` bigint(20) DEFAULT NULL,
-      `order_date` datetime(6) DEFAULT NULL,
-      `address` varchar(255) DEFAULT NULL,
-      `description` varchar(255) DEFAULT NULL,
-      PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+-- CREATE TABLE IF NOT EXISTS `${ORDER_TABLE}` (
+--       `quantity` int(11) NOT NULL,
+--       `customer_id` bigint(20) DEFAULT NULL,
+--       `id` bigint(20) NOT NULL AUTO_INCREMENT,
+--       `item_id` bigint(20) DEFAULT NULL,
+--       `order_date` datetime(6) DEFAULT NULL,
+--       `address` varchar(255) DEFAULT NULL,
+--       `description` varchar(255) DEFAULT NULL,
+--       PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 CREATE USER IF NOT EXISTS '${USER}'@'%' IDENTIFIED BY '${PASSWORD}';
 GRANT ALL PRIVILEGES ON `${ORDER_DATABASE}`.* TO '${USER}'@'%';
