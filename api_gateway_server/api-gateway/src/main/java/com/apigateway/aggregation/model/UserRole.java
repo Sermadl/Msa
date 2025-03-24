427f2903b23047c71b2db54107ef643529193ffa
Kim Se Eun <rlatpdms1014@gmail.com>
2025-03-24 17:25:16 +0900

    feat: 각 마이크로 서버에서 권한 별로 API 접근을 제한하기 위해 Header에 유저 정보 포함하기

--- api_gateway_server/api-gateway/src/main/java/com/apigateway/aggregation/model/UserRole.java
+++ api_gateway_server/api-gateway/src/main/java/com/apigateway/aggregation/model/UserRole.java
@@ -13,45 +13,11 @@ public enum UserRole {
     GUEST("GUEST"),
     USER("USER"),
     SELLER("SELLER"),
-    ADMIN(combine("ADMIN", "USER", "SELLER"));
+    ADMIN("ADMIN");
 
-    @JsonValue
     private final String name;
 
     UserRole(String name) {
         this.name = name;
-    }
-
-    public static String combine(String... names) {
-        return String.join(",", names);
-    }
-
-    private static final Map<String, UserRole> BY_LABEL =
-            Stream.of(values()).collect(Collectors.toMap(UserRole::getName, e -> e));
-
-    public static UserRole of(String name) {
-        return BY_LABEL.get(name);
-    }
-
-    public boolean isAdmin() {
-        return this == ADMIN;
-    }
-
-    public boolean isSeller() {
-        return this == SELLER;
-    }
-
-    public boolean isUser() {
-        return this == USER;
-    }
-
-    @JsonCreator
-    public static UserRole from(String val) {
-        for (UserRole role : UserRole.values()) {
-            if (role.name().equals(val)) {
-                return role;
-            }
-        }
-        return null;
     }
 }
